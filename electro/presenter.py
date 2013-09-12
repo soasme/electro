a621ffe7d8e22ce465ad9a86322f4668d347fef6
soasme <soasme@gmail.com>
2013-09-12 17:58:59 +0800

    Initialize presenter.

--- electro/presenter.py
+++ electro/presenter.py
@@ -4,6 +4,11 @@ class Presenter(object):
 
     mediatype = None
 
+    def __init__(self, data, code, headers):
+        self.data = data
+        self.code = code
+        self.headers = headers
+
     @classmethod
     def as_representation(cls, method, data, code, headers):
         presenter = cls(data, code, headers)
