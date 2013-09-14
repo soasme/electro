c6aefdd2aa581036b1a8db4430ba8baf3ccc9924
soasme <soasme@gmail.com>
2013-09-14 08:02:44 +0800

    Parse obtain object.

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
+from flask import Flask
 from electro.resource import Resource
+from electro.api import API
 
 class TestResource(TestCase):
 
@@ -14,16 +16,27 @@ class TestResource(TestCase):
         self.assert_parser(('', 204, {}), '')
 
     def test_dict_will_return_200(self):
-        self.assert_parser(('{}', 200, {}), {})
+        self.assert_parser(({}, 200, {}), {})
 
     def test_list_will_return_200(self):
-        self.assert_parser(('[]', 200, {}), [])
+        self.assert_parser(([], 200, {}), [])
 
     def test_str_will_return_200(self):
-        self.assert_parser(('"test"', 200, {}), 'test')
+        self.assert_parser(('test', 200, {}), 'test')
 
     def test_data_with_code(self):
-        self.assert_parser(('{}', 201, {}), ({}, 201))
+        self.assert_parser(({}, 201, {}), ({}, 201))
 
     def test_data_with_code_and_headers(self):
-        self.assert_parser(('{}', 201, {'k':'v'}), ({}, 201, {'k':'v'}))
+        self.assert_parser(({}, 201, {'k':'v'}), ({}, 201, {'k':'v'}))
+
+    def test_dispatch_HEAD_to_GET_if_head_method_is_not_defined(self):
+        app = Flask(__name__)
+        api = API(app)
+        class T(Resource):
+            def get(self): return ''
+        api.add_resource(T, '/test')
+
+        with app.test_client() as client:
+            response = client.head('/test')
+            print response
